cdbf1d0ca3a15fffcea7b6a791e963dcd8e8e6e4
Alex Mejias <alexdmejias@gmail.com>
2023-04-20 15:07:33 -0400

    server listens to ip4 ip

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import app from "./app";
 
 const start = async () => {
   try {
-    await app.listen({ port: 3000 });
+    await app.listen({ port: 3000, host: "0.0.0.0" });
   } catch (err) {
     app.log.error(err);
     process.exit(1);
