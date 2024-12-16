440dc427f614243667b4f099cc3274d477c0523c
Alex Mejias <alexdmejias@gmail.com>
2024-12-16 00:17:12 -0500

    small cleanups

--- index.ts
+++ index.ts
@@ -1,14 +1,10 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-// const app = require("./dist/app.js").default;
-import app from './src/app'
-
-// app.listen(3000, () => {
-//   console.log(`Example app listening on port ${3000}`);
-// });
+import app from "./src/app";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const start = async () => {
   try {
-    await app.listen({ port: 3000 });
+    await app.listen({ port: process.env.PORT || 3333, host: "0.0.0.0" });
   } catch (err) {
     app.log.error(err);
     process.exit(1);
