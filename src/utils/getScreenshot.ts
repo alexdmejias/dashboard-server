d120ead48e326ee9f3b2b873ac5e34194b9d3f83
Alex Mejias <alexdmejias@gmail.com>
2024-12-17 23:42:58 -0500

    moving to puppeteer

--- src/utils/getScreenshot.ts
+++ src/utils/getScreenshot.ts
@@ -1,4 +1,4 @@
-import { chromium } from "playwright";
+import puppeteer from "puppeteer";
 import imagesPath from "./imagesPath";
 import getRenderedTemplate from "./getRenderedTemplate";
 import { join } from "node:path";
@@ -10,12 +10,11 @@ async function getScreenshot<T>({
   template: string;
   data: T;
 }) {
-  const browser = await chromium.launch();
-  const context = await browser.newContext();
-  const page = await context.newPage();
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
 
   // TODO should be params
-  page.setViewportSize({
+  page.setViewport({
     width: 1200,
     height: 825,
   });
