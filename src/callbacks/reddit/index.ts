4fca041a5dcff3ecaac1940c791bbaf6c1356b7c
Alex Mejias <alexdmejias@gmail.com>
2025-11-16 15:58:09 -0500

    WIP

--- src/callbacks/reddit/index.ts
+++ src/callbacks/reddit/index.ts
@@ -17,8 +17,17 @@ type RedditPost = RedditResponseRoot["data"]["children"][number]["data"][];
 
 export const expectedConfig = z.object({
   subreddit: z.string(),
+  title: z.string().optional(),
   qty: z.number().positive(),
 });
+
+type t = z.infer<typeof expectedConfig>;
+
+const defaultClassOptions: t = {
+  title: "default reddit title",
+  qty: 3,
+  subreddit: "pets",
+};
 
 class CallbackReddit extends CallbackBase<RedditPost, typeof expectedConfig> {
   constructor(options = {}) {
@@ -31,18 +40,18 @@ class CallbackReddit extends CallbackBase<RedditPost, typeof expectedConfig> {
 
   async getData(config: z.infer<typeof expectedConfig>) {
     try {
-      const { qty = 10, subreddit = "asknyc" } = config;
+      const { qty, subreddit } = config;
 
       const dataRes = await fetch(
         `https://reddit.com/r/${subreddit}/new.json?sort=new&limit=${qty}`,
       );
 
       const json = (await dataRes.json()) as RedditResponseRoot;
-      const posts = json.data.children.map((p) => ({
+      const data = json.data.children.map((p) => ({
         title: p.data.title,
       }));
 
-      return posts;
+      return data;
     } catch (e) {
       return { error: e instanceof Error ? e.message : (e as string) };
     }
