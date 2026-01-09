fb83dbf3c08d752e977a3af3563c4f29045d404b
Alex Mejias <alexdmejias@gmail.com>
2026-01-09 13:47:00 -0500

    random

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,8 +3,6 @@ module.exports = {
     {
       name: "dashboard-server",
       script: "./dist/index.js",
-      // cwd is omitted - PM2 will use the directory from which it's started
-      // This makes the config portable and works with GitHub Actions
       instances: 1,
       autorestart: true,
       watch: false,
